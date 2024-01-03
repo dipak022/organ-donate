6e8f934619b0a134b3d2ccac85b5337c068ca159
dipak022 <dipakd3ebnath4002@gmail.com>
2024-01-03 12:01:31 +0600

    update

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="main-footer">
-    <strong>Developed By: <a href="https://www.facebook.com/Dipak.Debnath.2455/" target="blank">Dipak Debnath</a>.</strong>
-    <b>Contact: </b> 01865403461
+    <strong>Developed By: <a href="https://www.facebook.com/Dipak.Debnath.2455/" target="blank">Tonisa Adhikary
+</a>.</strong>
+    <b>Contact: </b> 01521440888
     <div class="float-right d-none d-sm-inline-block">
-        Copyright &copy; {{ date('Y') }} <a href="https://www.facebook.com/Dipak.Debnath.2455/" target="blank">Dipak Debnath</a> - All rights reserved.
+        Copyright &copy; {{ date('Y') }} <a href="https://www.facebook.com/Dipak.Debnath.2455/" target="blank">DTonisa Adhikary
+</a> - All rights reserved.
     </div>
 </footer>

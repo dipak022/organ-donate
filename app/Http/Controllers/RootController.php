695a13db812cd287e9d9d16bc4557ffd182af12a
dipak022 <dipakd3ebnath4002@gmail.com>
2024-01-07 17:07:48 +0600

    update

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -9,7 +9,7 @@ class RootController extends Controller
 {
     public function home(){
         $data['doctors'] = Doctor::with(['designation','department'])->get();
-        $data['donates'] = Donate::all();
+        $data['donates'] = Donate::where('use_status',1)->get();
         return view('frontend.index', $data);
     }
 
